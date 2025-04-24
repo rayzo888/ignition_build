631984287ebe0c4fc1b6d04cac1ac7046e29d96a
Shong Liew <rayzo98008@gmail.com>
2025-04-24 12:51:37 -0400

    added update tags for docker container

---
+++ build/update_tags.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Set variables
+UPDATE_TAGS_URL="http://ignition.local/system/webdev/HomeAutomation/utility/tagControl/updateTags"
+TAGFOLDER="/usr/local/bin/ignition/data/tags"
+CHECK_GWINFO_URL="http://ignition.local/system/gwinfo"
+
+echo "Waiting for Gateway to report RUNNING..."
+
+# Poll until the gateway is fully running
+while true; do
+  STATUS=$(curl -s "$CHECK_GWINFO_URL")
+  echo "$STATUS" | grep -q "ContextStatus=RUNNING"
+  if [ $? -eq 0 ]; then
+    echo "OK: Gateway is RUNNING. Continuing..."
+    break
+  else
+    echo "Waiting for Gateway status to fully RUNNING..."
+    sleep 3
+  fi
+done
+
+# Post tag folder path
+echo "Updating tags from tag folder..."
+curl -X POST \
+  -H "Accept: application/json" \
+  -H "Content-Type: application/json" \
+  -d "{\"tagFileFolder\":\"$TAGFOLDER\"}" \
+  "$UPDATE_TAGS_URL"
+
+echo "Script completed successfully."
+sleep 5
+exit 0
